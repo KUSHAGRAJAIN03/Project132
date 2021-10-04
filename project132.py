33851a0fdda725b64d9952018ffad14b9d3540be
KUSHAGRAJAIN03 <66012878+KUSHAGRAJAIN03@users.noreply.github.com>
2021-10-04 19:56:19 +0530

    Add files via upload

---
+++ project132.py
@@ -0,0 +1,23 @@
+from Pro131 import Gravity
+import pandas as pd
+import matplotlib.pyplot as plt 
+
+df = pd.read_csv("project_131.csv")
+radius = df["Radius"].tolist()
+mass = df["Mass"].tolist()
+gravity = df["Gravity"].tolist()
+
+mass.sort()
+radius.sort()
+gravity.sort()
+
+plt.plot(radius,mass)
+plt.xlabel("RADIUS")
+plt.ylabel("MASS")
+plt.title("RADIUS-MASS GRAPH")
+plt.show()
+plt.plot(radius,gravity)
+plt.xlabel("RADIUS")
+plt.ylabel("GRAVITY")
+plt.title("RADIUS-GRAVITY GRAPH")
+plt.show()
